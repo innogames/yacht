f745339e421a0e30fd99fc7ff099cfb768f387d5
Kajetan Staszkiewicz <kajetan.staszkiewicz@innogames.de>
2017-05-11 20:37:37 +0200

    Start pfctl development

---
+++ pfctl/pfctlcmd.go
@@ -0,0 +1,54 @@
+package pfctl
+
+import (
+	"bufio"
+	"github.com/innogames/yacht/logger"
+	"os/exec"
+	"strings"
+)
+
+type pfctlError struct {
+	s string
+}
+
+func (e pfctlError) Error() string {
+	return "pfctl: " + e.s
+}
+
+func pfctlCmd(arg []string) (*bufio.Scanner, error) {
+	cmd := exec.Command("/sbin/pfctl", arg...)
+	out, err := cmd.CombinedOutput()
+
+	outStr := string(out)
+
+	if err != nil {
+		return nil, pfctlError{err.Error() + outStr}
+	}
+
+	scanner := bufio.NewScanner(strings.NewReader(outStr))
+
+	return scanner, nil
+}
+
+func pfctlGetTable(table string) ([]string, error) {
+	var ret []string
+
+	out, err := pfctlCmd([]string{"-t", table, "-Ts"})
+	if err != nil {
+		return nil, err
+	}
+
+	for out.Scan() {
+		line := out.Text()
+		logger.Debug.Printf("got line '%s'", line)
+	}
+
+	return ret, nil
+}
+
+func pfctlSyncTable(table string, nodes []string) error {
+
+	_, err := pfctlGetTable(table)
+
+	return err
+}
