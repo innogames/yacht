d2d68f7c33a24811d58a729800dc2d56e2e5fe2c
Charlie Root <root@aw-hwlb-01.ig.local>
2017-05-11 22:26:26 +0200

    Provide function to update synchronize pf table

--- pfctl/pfctlcmd.go
+++ pfctl/pfctlcmd.go
@@ -41,16 +41,86 @@ func pfctlGetTable(table string) ([]net.IP, error) {
 	}
 
 	for out.Scan() {
-		line := out.Text()
-		logger.Debug.Printf("got line '%s'", line)
+		line := strings.Trim(out.Text(), " ")
+		if ipAddress := net.ParseIP(line); ipAddress != nil {
+			ret = append(ret, ipAddress)
+		}
 	}
 
 	return ret, nil
 }
 
-func pfctlSyncTable(table string, nodes []string) error {
+func pfctlDelFromTable(table string, ipAddresses []net.IP) error {
+	if len(ipAddresses) == 0 {
+		return nil
+	}
+	logger.Debug.Printf("deleting from %s: %s", table, ipAddresses)
+	cmd := []string{"-t", table, "-T", "del"}
+	for _, ipAddress := range ipAddresses {
+		cmd = append(cmd, ipAddress.String())
+	}
+	_, err := pfctlCmd(cmd)
+	return err
+}
 
-	_, err := pfctlGetTable(table)
+func pfctlAddToTable(table string, ipAddresses []net.IP) error {
+	if len(ipAddresses) == 0 {
+		return nil
+	}
+	logger.Debug.Printf("adding to %s: %s", table, ipAddresses)
+	cmd := []string{"-t", table, "-T", "add"}
+	for _, ipAddress := range ipAddresses {
+		cmd = append(cmd, ipAddress.String())
+	}
+	_, err := pfctlCmd(cmd)
+	return err
+}
+
+func pfctlSyncTable(table string, wantSet []net.IP) error {
+
+	var addSet, delSet []net.IP
+
+	curSet, err := pfctlGetTable(table)
+	if err != nil {
+		return err
+	}
+
+	logger.Debug.Printf("have in %s: %s", table, curSet)
+	logger.Debug.Printf("want in %s: %s", table, wantSet)
+
+	// Add wanted nodes.
+	for _, want := range wantSet {
+		found := false
+		for _, cur := range curSet {
+			if want.Equal(cur) {
+				found = true
+			}
+		}
+		if found == false {
+			addSet = append(addSet, want)
+		}
+	}
+
+	// Remove unwanted nodes.
+	for _, cur := range curSet {
+		found := false
+		for _, want := range wantSet {
+			if cur.Equal(want) {
+				found = true
+			}
+		}
+		if found == false {
+			delSet = append(delSet, cur)
+		}
+	}
+
+	if err := pfctlAddToTable(table, addSet); err != nil {
+		return err
+	}
+
+	if err := pfctlDelFromTable(table, delSet); err != nil {
+		return err
+	}
 
 	return err
 }
